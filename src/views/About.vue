2155ef0e41b07ceb9bf33ea59f36c40f2a98647b
Lucas Youngers <lucasyoungers@Adams-MacBook-Pro.local>
2019-04-05 12:36:50 -0700

    finished user.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="user">
+    <user-header></user-header>
+    <user-body></user-body>
+    <user-footer></user-footer>
   </div>
 </template>
+
+<script>
+import UserHeader from '@/components/UserHeader.vue'
+import UserBody from '@/components/UserBody.vue'
+import UserFooter from '@/components/UserFooter.vue'
+
+export default {
+  name: user,
+  components: {
+    UserHeader,
+    UserBody,
+    UserFooter
+  }
+}
+</script>
